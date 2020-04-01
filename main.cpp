e323c4c0907f10c3e70eb1745b6902cd487ac6db
d <ledaiduongvnth@gmail.com>
2020-04-01 16:41:02 +0700

    using unknown instead of ""

--- main.cpp
+++ main.cpp
@@ -133,12 +133,13 @@ public:
                         }
                         // end attach detection results to the trackers
                         // put text and draw rectangle
-                        cv::putText(display_image, it->name, cv::Point(pBox.x - 10*int(it->name.length()*this->fontScale/2), pBox.y),
+                        std::string displayName = (it->name.empty()) ? "unknown" : it->name;
+                        cv::putText(display_image, displayName, cv::Point(pBox.x - 10*int(it->name.length()*this->fontScale/2), pBox.y),
                                 cv::FONT_HERSHEY_SIMPLEX, this->fontScale, CV_RGB(0, 255, 0), 2);
                         cv::Rect rect = cv::Rect(pBox.x, pBox.y, pBox.width, pBox.height);
                         DrawRectangle(display_image, rect, 3, 3, CV_RGB(255, 255, 127));
                         // end put text and draw rectangle
-                        if (it->name == ""){
+                        if (it->name.empty()){
                             // get face image and landmarks to make request
                             tie(cropedImage, new_left, new_top) = CropFaceImageWithMargin(origin_image,
                                     pBox.x, pBox.y,pBox.x + pBox.width,pBox.y + pBox.height, 1.3);
